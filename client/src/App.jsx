878932960548be418ee7ad2c103b13689a8daa55
Vivek kumar <viveka9415@gmail.com>
2025-09-08 15:14:43 +0530

    refactor: standardize import formatting and clean up JSX structure

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,26 +1,21 @@
-import React from 'react'
-import Home from './Pages/Home'
-import Course from './Pages/Course'
-import {Routes,Route} from "react-router-dom"
-import Contact from './Pages/Contact'
-import About from './Pages/About'
-import Signup from './components/Signup'
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Home from "./Pages/Home";
+import Course from "./Pages/Course";
+import Contact from "./Pages/Contact";
+import About from "./Pages/About";
+import Signup from "./components/Signup";
 
 function App() {
   return (
-    <>
-    {/* <Home/>
-    <Course/> */}
     <Routes>
-      <Route path="/" element={<Home/>}/>
-      <Route path="/course" element={<Course/>}/>
-      <Route path="/contact" element={<Contact/>}/>
-      <Route path="/about" element={<About/>}/>
-      <Route path="/signup" element={<Signup/>}/>
-
+      <Route path="/" element={<Home />} />
+      <Route path="/course" element={<Course />} />
+      <Route path="/contact" element={<Contact />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/signup" element={<Signup />} />
     </Routes>
-    </>
-  )
+  );
 }
 
-export default App
+export default App;
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,11 +1,11 @@
-import { BrowserRouter } from 'react-router-dom'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App'
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
+import "./index.css";
 
-
-createRoot(document.getElementById('root')).render(
-< BrowserRouter>
-<App/>
-</BrowserRouter>,
-)
+createRoot(document.getElementById("root")).render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+);
